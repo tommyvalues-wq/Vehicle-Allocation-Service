b178d8b184fff6343b501f0f0581d6ed3981c7d4
tommyvalues-wq <tommyvalues@gmail.com>
2026-06-06 21:56:51 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,119 @@
+/* ==========================================================
+   Vehicle Allocation Simulator v2.0
+   app.js
+   Main simulator bootstrap
+========================================================== */
 
+const App = {
+
+    version: "2.0",
+
+    started: false,
+
+    currentTown: null,
+
+    fleet: [],
+
+    selectedUnit: null,
+
+    map: null,
+
+    streetLayer: null,
+
+    satelliteLayer: null,
+
+    landmarkLayer: null,
+
+    hospitalLayer: null,
+
+    stationLayer: null,
+
+    messageQueue: [],
+
+    unreadMessages: [],
+
+    codeZeroActive: false,
+
+    timers: {}
+
+};
+
+document.addEventListener("DOMContentLoaded", bootSimulator);
+
+function bootSimulator(){
+
+    console.log("Vehicle Allocation Simulator v2.0");
+
+    App.started=true;
+
+    initialiseMap();
+
+    initialiseFleet();
+
+    initialiseDispatcher();
+
+    initialiseChat();
+
+    initialiseIncidents();
+
+    initialiseAudio();
+
+    initialiseCode0();
+
+    startMainLoop();
+
+}
+
+function startMainLoop(){
+
+    App.timers.clock=
+        setInterval(updateSimulator,1000);
+
+    App.timers.friendMessages=
+        setInterval(randomFriendMessage,1800000);
+
+    App.timers.jobs=
+        setInterval(generateRandomIncident,90000);
+
+    App.timers.code0=
+        setInterval(checkForCodeZero,600000);
+
+    App.timers.updates=
+        setInterval(sendRandomCrewUpdate,480000);
+
+}
+
+function updateSimulator(){
+
+    updateFleetMovement();
+
+    updateIncidentProgress();
+
+    updateChatTimers();
+
+    updateDispatcherUI();
+
+}
+
+function simulatorTime(){
+
+    return new Date().toLocaleTimeString(
+        [],
+        {
+            hour:'2-digit',
+            minute:'2-digit'
+        }
+    );
+
+}
+
+function log(text){
+
+    console.log(
+        "["+
+        simulatorTime()+
+        "] "+
+        text
+    );
+
+}
